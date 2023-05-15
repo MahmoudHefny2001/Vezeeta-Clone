416f3b5c1e1aabf436e6882e380cb595f7170730
MahmoudHefny2001 <hefny4@gmail.com>
2023-05-16 01:14:09 +0300

    Considered doctors views config

--- doctor/views.py
+++ doctor/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Doctor
+from .serializers import DoctorSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
+from rest_framework import viewsets, generics, mixins, views
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class DoctorRegistrationView(views.APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = DoctorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = [AllowAny, IsAuthenticatedOrReadOnly]
+    # Define the allowed methods for users
+    http_method_names = ['get', 'head', 'options']
+
+class DoctorSelfViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = [IsAuthenticated]
+    # Define the allowed methods for doctors themselves
+    http_method_names = ['get', 'head', 'post', 'put', 'patch', 'delete']
