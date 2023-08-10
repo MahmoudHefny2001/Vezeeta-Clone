8f5a861154a1aa4090f6346c633071b5df3327ae
Mahmoud Hefny <66621768+MahmoudHefny2001@users.noreply.github.com>
2023-08-10 04:50:01 +0300

    Update admin.py

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Person
+  
+ admin.site.register(Person)
+  
+ class PersonModelAdmin(admin.ModelAdmin): 
+     def save_model(self, request, obj, form, change): 
+         # Hash the password before saving the object 
+         obj.set_password(form.cleaned_data['password']) 
+         super().save_model(request, obj, form, change)
