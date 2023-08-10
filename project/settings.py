3d2f099d140dfb2e284e779ebe88318ebf7847dc
Mahmoud Hefny <hefny4@gmail.com>
2023-08-10 08:55:33 +0300

    Pre pull mobile changes

--- project/settings.py
+++ project/settings.py
@@ -16,6 +16,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY", None)
 DEBUG = bool(os.environ.get("DEBUG", None))
 
 
+# For production
+
+if DEBUG:
+    DEBUG = False
+
 ALLOWED_HOSTS = list(str(os.environ.get("ALLOWED_HOSTS")).split(", "))
 
 
