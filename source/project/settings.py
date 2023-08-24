4f096a925f9b7dc518d11b3945f1504e1fa521cc
Mahmoud Hefny <hefny4@gmail.com>
2023-08-25 01:50:11 +0300

    Test 4 Railway volume path

--- source/project/settings.py
+++ source/project/settings.py
@@ -234,9 +234,13 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 
 # Railway Volume static files
-STATIC_URL = "/vezeeta/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, '/vezeeta/static')
-STATIC_FILES_DIRS = [BASE_DIR / "/vezeeta/static",]
+STATIC_URL = "/Vezeeta-Clone/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_FILES_DIRS = [BASE_DIR / "static/",]
+
+# Railway Volume media files
+MEDIA_URL = "/Vezeeta-Clone/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 
 # PRODUCTION STATIC FILES
@@ -250,11 +254,6 @@ STATIC_FILES_DIRS = [BASE_DIR / "/vezeeta/static",]
 # Loacl media files
 # MEDIA_URL = "/media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-
-
-# Railway Volume media files
-MEDIA_URL = "/vezeeta/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "/vezeeta/media/")
 
 
 # PRODUCTION MEDIA FILES
