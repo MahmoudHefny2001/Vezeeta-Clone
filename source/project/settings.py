6ada35459fb17679ac21f7455f402310526a5e12
Mahmoud Hefny <hefny4@gmail.com>
2023-08-25 06:41:08 +0300

    Test 27 Railway volume path

--- source/project/settings.py
+++ source/project/settings.py
@@ -246,8 +246,8 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-RAILWAY_VOLUME_NAME = str(os.environ.get("RAILWAY_VOLUME_NAME"))
-RAILWAY_VOLUME_MOUNT_PATH = str(os.environ.get("RAILWAY_VOLUME_MOUNT_PATH"))
+RAILWAY_VOLUME_NAME = os.environ.get("RAILWAY_VOLUME_NAME")
+RAILWAY_VOLUME_MOUNT_PATH = os.environ.get("RAILWAY_VOLUME_MOUNT_PATH")
 
 # Configure static files serving
 
