7a4bfd4a517fa3e241769573f8bd3f5ece046517
Mahmoud Hefny <hefny4@gmail.com>
2023-09-09 01:57:57 +0300

    Doctor Profiles Editing Done

--- source/doctor/permissions.py
+++ source/doctor/permissions.py
@@ -2,6 +2,27 @@ from rest_framework.permissions import BasePermission
 from .models import DoctorProfile, DoctorExtended 
 from person.models import Person
 
-class IsProfileOwner(BasePermission):  
-    pass
-    
+from rest_framework.permissions import SAFE_METHODS
+
+class IsObjectOwnerOrReadOnly(BasePermission):
+    """
+    Custom permission to allow read-only access to any user (including anonymous)
+    while restricting other methods (PUT, PATCH, DELETE, etc.) to the object's owner.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+ 
+        if request.method in SAFE_METHODS:
+            return True
+
+        # print("User: ", request.user.email, "Object: " , obj)
+        return obj.doctor.email == request.user.email
